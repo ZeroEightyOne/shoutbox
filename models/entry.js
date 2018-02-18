57f05decf50e67cd82676db0f9c07145499142b5
mark081 <cooper.canada@gmail.com>
2018-02-17 21:05:36 -0800

    Inital commit

---
+++ models/entry.js
@@ -0,0 +1,33 @@
+const redis = require('redis');
+const db = redis.createClient();
+
+class Entry {
+    constructor(obj) {
+        for(let key in obj) {
+            this[key] = obj[key]
+        }
+    }
+
+    save(callback) {
+        const entryJSON = JSON.stringify(this);
+        db.lpush (
+            'entries', entryJSON, (err) => {
+                if (err) return callback(err);
+                callback();
+            }
+
+        );
+    }
+    static getRange(from, to, callback) {
+        db.lrange('entries', from, to, (err,items) => {
+            if (err) return callback(err);
+            let entries = [];
+            items.forEach((item) => {
+                entries.push(JSON.parse(item));
+            });
+            callback(null, entries);
+        });
+    }
+}
+
+module.exports = Entry;
---
+++ models/user.js
@@ -0,0 +1,86 @@
+const redis = require('redis');
+const bcrypt = require('bcrypt');
+const db = redis.createClient();
+
+class User {
+    constructor(obj) {
+        for (let key in obj) {
+            this[key] = obj[key];
+        }
+    }
+    static getByName(name,callback) {
+        User.getId(name,(err,id) => {
+            if (err) return callback(err);
+            User.get(id,callback);
+        });
+    }
+    static getId(name, callback){
+        db.get(`user:id:${name}`,callback);
+    }
+
+    static get(id,callback) {
+        db.hgetall(`user:${id}`, (err,user) => {
+            if(err) return callback(err);
+            callback(null, new User(user));
+        });
+    }
+    static authenticate(name, pass, callback) {
+        User.getByName(name, (err, user) => {
+            if (err) return callback(err);
+            if(!user.id) return callback();
+            bcrypt.hash(pass, user.salt, (err, hash) => {
+                if(err) return callback(err);
+                if(hash == user.pass) return callback(null, user);
+                callback();
+            });
+        });
+    }
+
+
+    save(callback) {
+        if(this.id) {
+            this.update(callback);
+        } else {
+            db.incr('user:ids',(err,id) => {
+                if(err) return callback(err);
+                this.id = id;
+                this.hashPassword((err) => {
+                    if(err) return callback(err);
+                    this.update(callback);
+                });
+            });
+        }
+    }
+    update(callback) {
+        const id = this.id;
+        db.set(`user:id:${this.name}`,id,(err) => {
+            if(err) return callback(err);
+            db.hmset(`user:${id}`, this, (err) => {
+                callback(err);
+            });
+        });
+    }
+    hashPassword(callback) {
+        bcrypt.genSalt(12, (err,salt) => {
+            if (err) return callback(err);
+            bcrypt.hash(this.pass, salt, (err,hash) => {
+                if (err) return callback(err);
+                this.pass = hash;
+                callback();
+            });
+        });
+    }
+}
+
+module.exports = User;
+
+//const TestUser = require('./models/user');
+const user = new User({ name: 'Example', pass: 'test'});
+user.save((err) => {
+    if(err) console.error(err);
+    console.log(`user id ${user.id}`);
+});
+
+User.getByName('Example', (err, user) => {
+    console.log(user);
+})
